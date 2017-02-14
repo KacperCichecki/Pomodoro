82234310fc282a239841c903d977baacd7b6a2cd
Kacper Cichecki <kacper.cichecki.90@gmail.com>
2017-02-14 02:21:41 +0100

    add connection to local data base mysql with username root and password cichach1 at this moment app sends time of starting and enging study and starting of relax but does'n work properly

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,32 +8,36 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
 	Parent root;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			root = FXMLLoader.load(Main.class.getResource("Sample.fxml"));
 
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Problem with FXMLLoader: cant load parent");
 		}
 
 		Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.sizeToScene();
-			primaryStage.setResizable(false);
-			primaryStage.setTitle("Pomadoro");
-			primaryStage.getIcons().add(new Image(getClass().getResource("pomodoro.png").toString()));
-			primaryStage.show();
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Pomadoro");
+		primaryStage.getIcons().add(new Image(getClass().getResource("pomodoro.png").toString()));
+		primaryStage.show();
 	}
 
-	public void stop(){
-		if (Controller.threadExecutor != null){
+	public void stop() {
+		if (Controller.threadExecutor != null) {
 			Controller.threadExecutor.shutdown();
 		}
+		if (Controller.dbConnetion != null) {
+			Controller.dbConnetion.closeConnection();
+		}
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
